e7ec89abb669377a1d51e3d17c8e2f841470783a
M J SWARNNAPADINI <swarnaaa1005@gmail.com>
2026-02-22 17:54:48 +0530

    Blockchain connection established-Certificate Issuance

--- backend/blockchain/contractAddress.cjs
+++ backend/blockchain/contractAddress.cjs
@@ -1,3 +1,3 @@
-const CONTRACT_ADDRESS = "0x22758b6b4212de3247f97b18194Cb2D28D1503fe";
+const CONTRACT_ADDRESS = "0x22758b6b4212de3247f97b18194Cb2D28D1503fe"; /*deployed address on ganache*/
 
 module.exports = { CONTRACT_ADDRESS };
